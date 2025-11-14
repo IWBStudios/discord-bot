6c0aa64114378a6eed41b53373f89cb58c41a20d
ItsWizZard <oddwizzard@gmail.com>
2025-11-14 13:42:12 +0000

    feat: add clientReady event with command redeployment

---
+++ bot/src/events/onClientReady.ts
@@ -0,0 +1,24 @@
+import { ActivityType } from 'discord.js';
+import type { BotEvent } from 'types/BotEvent';
+import { deployCommands } from '../handlers/deployCommands';
+
+const event: BotEvent<'clientReady'> = {
+  name: 'clientReady',
+  run: async (client) => {
+    await deployCommands(client);
+    console.log(`Logged in as ${client.user?.tag}`);
+
+    client.user?.setPresence({
+      activities: [
+        {
+          name: 'Under Development',
+          type: ActivityType.Custom,
+          state: 'Something good is cooking...',
+        },
+      ],
+      status: 'online',
+    });
+  },
+};
+
+export default event;
