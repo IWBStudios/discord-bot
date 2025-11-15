ad98c85d45f4f5554380e66404c4852611e3386b
ItsWizZard <oddwizzard@gmail.com>
2025-11-15 06:09:27 +0000

    chore: remove unused export

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -2,7 +2,7 @@ import { Client, GatewayIntentBits } from 'discord.js';
 import Config from './config';
 import { registerEvents } from './handlers/eventRegistry';
 
-export const client = new Client({
+const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMembers,
